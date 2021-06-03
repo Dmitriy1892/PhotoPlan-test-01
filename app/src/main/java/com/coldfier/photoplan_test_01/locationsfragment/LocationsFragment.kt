16394bc762b59508c62676e9c3c5a9629afc3bb6
ColdFier <kodim1892@gmail.com>
2021-06-03 23:51:35 +0300

    Not working, Firebase test successful, ready to implement image adding.

--- app/src/main/java/com/coldfier/photoplan_test_01/locationsfragment/LocationsFragment.kt
+++ app/src/main/java/com/coldfier/photoplan_test_01/locationsfragment/LocationsFragment.kt
@@ -1,16 +1,31 @@
 package com.coldfier.photoplan_test_01.locationsfragment
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.graphics.decodeBitmap
+import androidx.lifecycle.Observer
 import com.coldfier.photoplan_test_01.R
 import com.coldfier.photoplan_test_01.databinding.LocationsFragmentBinding
 
 class LocationsFragment : Fragment() {
+
+    private val IMAGE_RESPONSE = 1
 
     companion object {
         fun newInstance() = LocationsFragment()
@@ -18,6 +33,16 @@ class LocationsFragment : Fragment() {
 
     private lateinit var viewModel: LocationsViewModel
     private lateinit var binding: LocationsFragmentBinding
+    private lateinit var getContent: ActivityResultLauncher<String>
+    private var bitmap: Bitmap? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getContent = registerForActivityResult(ActivityResultContracts.GetContent()) {
+            val src = ImageDecoder.createSource(requireActivity().contentResolver, it)
+            bitmap = ImageDecoder.decodeBitmap(src)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,6 +61,15 @@ class LocationsFragment : Fragment() {
             viewModel.testClick()
             Toast.makeText(context, "Go to firebase!", Toast.LENGTH_SHORT).show()
         }
+        binding.include.floatingActionButton.setOnClickListener {
+            //viewModel.getClick()
+            getContent.launch("image/*")
+        }
+
+        viewModel.data.observe(viewLifecycleOwner, Observer {
+            binding.include.contentNameEditText.setText(it.toString())
+        })
+        if (bitmap != null) binding.imageView2.setImageBitmap(bitmap)
     }
 
 }
--- app/src/main/java/com/coldfier/photoplan_test_01/locationsfragment/LocationsViewModel.kt
+++ app/src/main/java/com/coldfier/photoplan_test_01/locationsfragment/LocationsViewModel.kt
@@ -1,11 +1,21 @@
 package com.coldfier.photoplan_test_01.locationsfragment
 
+import android.content.Intent
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObjects
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
 
 class LocationsViewModel : ViewModel() {
+
+    val data = MutableLiveData<MutableMap<String, Int>>()
+
     fun testClick() {
         val db = Firebase.firestore
 
@@ -17,5 +27,31 @@ class LocationsViewModel : ViewModel() {
             }.addOnFailureListener{
                 Log.d("fire", "fail")
             }
+    }
+
+    fun getClick() {
+        viewModelScope.launch {
+            val db = Firebase.firestore
+
+            val map = db.collection("users")
+                .get()
+                .addOnSuccessListener { snapshot ->
+                    run {
+                        Log.d("fire", "success")
+                        val mappp = mutableMapOf<String, Int>()
+                        snapshot.documents[0]
+                            .data?.forEach {
+                                    (key, value) ->
+                                mappp[key] = value.toString().toInt()
+                            }
+                        data.value = mappp
+                    }
+
+                }.addOnFailureListener{
+                    Log.d("fire", "fail")
+                }
+        }
+
+
     }
 }
